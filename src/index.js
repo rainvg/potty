01b20010eed51c13594507213ff4d4194229ee52
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2016-02-14 19:16:13 +0100

    Delegated all timing operations to nappy.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
-var request = require('request');
+var nappy = require('nappy');
 var nodegit = require('nodegit');
 var rimraf = require('rimraf');
 var mkdirp = require('mkdirp');
 var confio = require('confio');
 var path = require('path');
-
-var wait = {
-  for: function(milliseconds)
-  {
-    'use strict';
-    return milliseconds < 0 ? Promise.resolve() : new Promise(function(resolve)
-    {
-        setTimeout(resolve, milliseconds);
-    });
-  },
-  till: function(timestamp)
-  {
-    'use strict';
-    return wait.for(timestamp - new Date().getTime());
-  },
-  connection: function()
-  {
-    'use strict';
-    var settings = {retry: 1000};
-
-    return new Promise(function(resolve)
-    {
-      (function poll()
-      {
-        request('https://api.ipify.org', function(error, response, body)
-        {
-          if(!error && response.statusCode === 200 && /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/.test(body))
-            resolve();
-          else
-            wait.for(settings.retry).then(poll);
-        });
-      })();
-    });
-  }
-};
 
 function pot(root, repository, branch)
 {
@@ -124,7 +89,7 @@ function pot(root, repository, branch)
           return __setup_try__(_path.resources);
         }).then(resolve).catch(function()
         {
-          wait.for(settings.path_setup.retry).then(loop);
+          nappy.wait.for(settings.path_setup.retry).then(loop);
         });
       })();
     });
@@ -134,7 +99,7 @@ function pot(root, repository, branch)
   {
     var __setup_try__ = function()
     {
-      return wait.connection().then(function()
+      return nappy.wait.connection().then(function()
       {
         return nodegit.Clone(_repository, _path.app, {checkoutBranch: _branch.local});
       });
@@ -144,7 +109,7 @@ function pot(root, repository, branch)
     {
       return new Promise(function(resolve)
       {
-        wait.till(_config.get('setup_last') + Math.min(Math.pow(2, _config.get('setup_retries')) * settings.setup.retry.min, settings.setup.retry.max)).then(function()
+        nappy.wait.till(_config.get('setup_last') + Math.min(Math.pow(2, _config.get('setup_retries')) * settings.setup.retry.min, settings.setup.retry.max)).then(function()
         {
           try
           {
@@ -175,7 +140,7 @@ function pot(root, repository, branch)
   {
     var __pull_try__ = function()
     {
-      return wait.connection().then(function()
+      return nappy.wait.connection().then(function()
       {
         return nodegit.Repository.open(_path.app);
       }).then(function(repository)
