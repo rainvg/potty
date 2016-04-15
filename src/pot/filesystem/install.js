155ba908fc7afe29e5b22b95be58a67f4e9781d8
Marco Moschettini <m.marmos@gmail.com>
2016-04-16 01:00:49 +0200

    Added install module. I'm tired, needs revision.

---
+++ src/pot/filesystem/install.js
@@ -0,0 +1,34 @@
+var __path__ = require('./path.js');
+var unzip = require('./unzip.js');
+var __update__ = require('../network/update.js');
+var __app__ = require('../app/');
+var __config__ = require('../config');
+var __remote__ = require('../network/remote.js');
+
+module.exports = function install(path, zip, app, config, remote)
+{
+  //TODO: needs revision!
+
+  if(!(path instanceof __path__))
+    throw {code: 1, description: 'An instance of path is required.', url: ''};
+
+  if(!(app instanceof __app__))
+    throw {code: 1, description: 'An instance of app is required.', url: ''};
+
+  if(!(config instanceof __config__))
+    throw {code: 1, description: 'An instance of config is required.', url: ''};
+
+  if(!(remote instanceof __remote__))
+    throw {code: 1, description: 'An instance of remote is required.', url: ''};
+
+  return new Promise(function(resolve)
+  {
+    unzip(path, zip).then(function()
+    {
+      resolve(true);
+    }).catch(function()
+    {
+      __update__(app, path, config, remote, true).then(resolve);
+    });
+  });
+};
